1b1a05001ed548de7d5249ae147c38fc70e5871a
burningman2k20 <burningman2k9@gmail.com>
2021-01-28 14:10:00 -0500

    google sign in now works

--- Recipe/app/src/main/java/com/example/recipe/Login_SignUp.kt
+++ Recipe/app/src/main/java/com/example/recipe/Login_SignUp.kt
@@ -3,11 +3,18 @@ package com.example.recipe
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.recipe.List.Companion.newInstance
+import com.example.recipe.R.id.home2
+import com.example.recipe.R.id.login_SignUp
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -18,6 +25,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.fragment_login_sign_up.*
+import androidx.fragment.app.FragmentManager as AndroidxFragmentAppFragmentManager
+import com.example.recipe.List as RecipeList
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,6 +40,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class Login_SignUp : Fragment() {
     var thiscontext: Context? = null
+    var contain : Int = 0
     //var view : View? = null
     //var google_button : SignInButton? = null
     private lateinit var firebaseAuth: FirebaseAuth
@@ -69,6 +79,7 @@ class Login_SignUp : Fragment() {
            if (container != null) {
                thiscontext = container.context
            }
+
            return inflater.inflate(R.layout.fragment_login_sign_up, container, false)     // Inflate the layout for this fragment
         //return view
     }
@@ -130,11 +141,20 @@ class Login_SignUp : Fragment() {
         firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
             if (it.isSuccessful) {
                 val user = FirebaseAuth.getInstance().currentUser
-                Log.d("SUC",user.toString())
-                getFragmentManager()
-                    ?.beginTransaction()
-                    ?.replace(R.id.container, Home.newInstance("", ""))
-                    ?.commit();
+                Log.d("SUC", user?.displayName!!)
+                //var fragmentManager =
+                //val intent = RecipeList.newInstance("","").activity?.intent
+                //if (intent != null) startActivity(intent)
+//                parentFragmentManager.commit {
+//                    replace<RecipeList>(R.layout.fragment_login_sign_up)
+//                    setReorderingAllowed(true)
+//                    addToBackStack("name") // name can be null
+//                }
+//                getFragmentManager()
+////                //supportFragmentManager
+//                    ?.beginTransaction()
+//                    ?.replace(R.id.container, List.Companion.newInstance("", ""))
+//                    ?.commit();
                 //findNavController(view.).navigate(R.id.home2)
             }
              //   startActivity(Home.getLaunchIntent(this))
@@ -173,6 +193,7 @@ class Login_SignUp : Fragment() {
                     firebaseAuthWithGoogle(account)
                 }
             } catch (e: ApiException) {
+                Log.d("ERROR",e.toString())
                 //Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
             }
         }
