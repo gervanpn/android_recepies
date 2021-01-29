419be87b8f052912a6454356e57d03d4f7ed002d
burningman2k20 <burningman2k9@gmail.com>
2021-01-29 11:10:19 -0500

    Update Login_SignUp.kt

--- Recipe/app/src/main/java/com/example/recipe/Login_SignUp.kt
+++ Recipe/app/src/main/java/com/example/recipe/Login_SignUp.kt
@@ -116,7 +116,7 @@ class Login_SignUp : Fragment() {
     }
     private fun configureGoogleSignIn() {
         mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.web_client_id))
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         mGoogleSignInClient = GoogleSignIn.getClient(thiscontext!!, mGoogleSignInOptions)
