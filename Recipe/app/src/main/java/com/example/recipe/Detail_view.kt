e94bb6162137df80bbe2e99782e49ad08f2882da
mojfazel <fazeli.mojtaba@gmail.com>
2021-01-31 09:20:42 -0500

    clean up controller

--- Recipe/app/src/main/java/com/example/recipe/Detail_view.kt
+++ Recipe/app/src/main/java/com/example/recipe/Detail_view.kt
@@ -2,11 +2,9 @@ package com.example.recipe
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.google.android.material.snackbar.Snackbar
 
@@ -38,8 +36,6 @@ class Detail_view : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-//        val binding = DataBindingUtil.inflate<FragmentDetailViewBinding>(inflater, R.layout.fragment_detail_view, container, false)
         return inflater.inflate(R.layout.fragment_detail_view, container, false)
 
     }
