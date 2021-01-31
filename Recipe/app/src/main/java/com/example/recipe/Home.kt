24dec41340218e2c88c66acbc0a02ef23629107f
Gervan Nantel <gervann@yahoo.ca>
2021-01-31 09:27:39 -0500

    Linked up the about menu

--- Recipe/app/src/main/java/com/example/recipe/Home.kt
+++ Recipe/app/src/main/java/com/example/recipe/Home.kt
@@ -3,12 +3,12 @@ package com.example.recipe
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.recipe.databinding.FragmentHomeBinding
 
 
@@ -29,8 +29,18 @@ class Home : Fragment() {
         binding.btnEntree.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_home_to_list)
         )
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, requireView(). findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
