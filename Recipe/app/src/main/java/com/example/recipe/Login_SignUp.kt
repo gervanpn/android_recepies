681b2d90a79f90f1a03047a20dbb7059df1a4d7c
RoaaAlmadi <roaa.almadi@gmail.com>
2021-01-31 21:52:10 -0800

    changes the cards layout and added recipies to the database

--- Recipe/app/src/main/java/com/example/recipe/Login_SignUp.kt
+++ Recipe/app/src/main/java/com/example/recipe/Login_SignUp.kt
@@ -23,9 +23,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-
 
 
 class Login_SignUp : Fragment() {
