0e250188380566f7ac42a0110a385dee99dc26c9
burningman2k20 <burningman2k9@gmail.com>
2021-02-03 10:44:03 -0500

    Update AboutApp.kt

--- Recipe/app/src/main/java/com/example/recipe/AboutApp.kt
+++ Recipe/app/src/main/java/com/example/recipe/AboutApp.kt
@@ -5,21 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.inflate
+import com.example.recipe.databinding.FragmentAboutBinding
+import com.example.recipe.databinding.FragmentLoginSignUpBinding
 
 
 class AboutApp : Fragment() {
 
+    private lateinit var binding : FragmentAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_about,
+            container, false )
 
-        return inflater.inflate(R.layout.fragment_about, container, false)
+
+        /*myToolbar.setNavigationOnClickListener { view ->
+            // Navigate somewhere
+        }*/
+
+        return binding.root
+        //inflater.inflate(R.layout.fragment_about, container, false)
     }
+
+
 }
 
