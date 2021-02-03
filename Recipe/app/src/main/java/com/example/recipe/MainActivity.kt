0b0aaf20d6977a6b0ece52aeb53827b8463ad9bd
burningman2k20 <burningman2k9@gmail.com>
2021-02-03 15:36:29 -0500

    Update MainActivity.kt

--- Recipe/app/src/main/java/com/example/recipe/MainActivity.kt
+++ Recipe/app/src/main/java/com/example/recipe/MainActivity.kt
@@ -25,13 +25,13 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         val navController = findNavController(R.id.fragment)
         bottomNavigationView.setupWithNavController(navController)
-        val appBarConfiguration = AppBarConfiguration(setOf( R.id.home, R.id.login_SignUp, R.id.list, R.id.detail_view))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.aboutFragment, R.id.home, R.id.login_SignUp, R.id.list, R.id.detail_view))
         setupActionBarWithNavController(navController, appBarConfiguration)
         // Set Status Bar Color & transparency
         val window: Window = this@MainActivity.window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         // window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS) // deprecated
-        // Window.setStatusBarColor(R.color.green) // for API 30+
+        //Window.setStatusBarColor(R.color.green) // for API 30+
         window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.top_bar)
     }
 
