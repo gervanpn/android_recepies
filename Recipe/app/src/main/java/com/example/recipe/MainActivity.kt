212121c14d40b0daa5baaeaee9ea484e7762a9fe
burningman2k20 <burningman2k9@gmail.com>
2021-02-03 10:19:41 -0500

    trying to get ack button on aboout screen title bar to work

--- Recipe/app/src/main/java/com/example/recipe/MainActivity.kt
+++ Recipe/app/src/main/java/com/example/recipe/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.recipe
       
 
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import androidx.annotation.RequiresApi
@@ -21,10 +23,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
 
+    @SuppressLint("RestrictedApi")
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        R.id.action_aboutFragment_to_home
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         val navController = findNavController(R.id.fragment)
         bottomNavigationView.setupWithNavController(navController)
@@ -36,6 +40,25 @@ class MainActivity : AppCompatActivity() {
         // window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS) // deprecated
         // Window.setStatusBarColor(R.color.green) // for API 30+
         window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.top_bar)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            Log.d("ABOUT", destination.id.toString())
+            //id.toString())
+            if (destination.id == R.id.action_aboutFragment_to_home) Log.d("ABOUT", "Go Home")
+            /*findNavController().navigate(
+                                    R.id.nav_host_fragment_container
+                                    //bundleOf(Pair("toolbar_title", "My Details Fragment Title"))
+                                )*/
+
+            /* R.id.navigation_home -> "My title"
+                R.id.navigation_task_start -> "My title2"
+                R.id.navigation_task_finish -> "My title3"
+                R.id.navigation_status -> "My title3"
+                R.id.navigation_settings -> "My title4"
+                else -> "Default title"*/
+        //}
+
+        }
     }
 
 
