0b7457ce7ee9cdfbda0e16e1a21b42907f91e21a
Gervan Nantel <gervann@yahoo.ca>
2021-02-03 13:13:29 -0500

    working on the list page features

--- Recipe/app/src/main/java/com/example/recipe/List.kt
+++ Recipe/app/src/main/java/com/example/recipe/List.kt
@@ -1,6 +1,7 @@
 package com.example.recipe
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -8,21 +9,24 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipe.databinding.FragmentListBinding
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
 
 class List : Fragment() {
 
     private lateinit var binding: FragmentListBinding
+//    private lateinit var firebaseAuth: FirebaseAuth
+//    val user = firebaseAuth.currentUser!!.email
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-
+//        var firebaseAuth = FirebaseAuth.getInstance()
+//        checkSignInStatus()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_list, container, false)
         binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
         binding.recyclerView.adapter = RecipeAdapter()
@@ -40,6 +44,17 @@ class List : Fragment() {
         return NavigationUI.onNavDestinationSelected(item, requireView(). findNavController())
                 || super.onOptionsItemSelected(item)
     }
+
+//    fun checkSignInStatus(){
+//        if (firebaseAuth.currentUser != null) {
+//            setHasOptionsMenu(true)
+//            Log.d("Tag", "Signed in")
+//        } else {
+////            setHasOptionsMenu(false)
+//            Log.d("Tag", "Not signed in")
+//        }
+//
+//    }
 
 
 }
