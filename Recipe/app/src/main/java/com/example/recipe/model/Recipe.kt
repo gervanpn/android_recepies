aaa4e02e187d364e40e1e78cbdf17d26317eb80b
Gervan Nantel <gervann@yahoo.ca>
2021-02-22 14:44:15 -0500

    clean up spaces

--- Recipe/app/src/main/java/com/example/recipe/model/Recipe.kt
+++ Recipe/app/src/main/java/com/example/recipe/model/Recipe.kt
@@ -5,24 +5,15 @@ import android.os.Bundle
 class Recipe constructor() {
     
     var recipe_instructions: String? = ""
-    
     var recipe_category: String? = ""
-    
     var recipe_detail: String? = ""
-       
     var recipe_id: String? = ""
         get() = this.recipe_id
-
     var recipe_ingredients: String? = ""
-
     var recipe_name: String? = ""
-
     var recipe_picture: String? = ""
-
     var user_id: String? = ""
-
     var recipe_status: Boolean = false
-
     companion object {
         var recipeSelected = Bundle()
     }
